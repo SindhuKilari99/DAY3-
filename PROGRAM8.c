9c52fca1be9a8c2e81cbd0ef587664cf43ee4851
SindhuKilari99 <108974193+SindhuKilari99@users.noreply.github.com>
2022-07-14 14:45:42 +0530

    Update PROGRAM8.c

--- PROGRAM8.c
+++ PROGRAM8.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-
-	char ch;
-
-	scanf("%c",&ch);
-
-	((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))?printf("is alphabet"):printf("is not an alphabet");
+    char ch;
+    printf("enter a character ");
+    scanf("%c",&ch);
+    (ch>=65&&ch<=91)||(ch>=97&&ch<=122) ? printf("alphabet") : printf("not alphabet");
 }
